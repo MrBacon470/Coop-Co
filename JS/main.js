7e56b11595129cb1f0f770bd3361e74a8134e9cd
MrBacon470 <Mrbacon470@outlook.com>
2022-05-23 23:12:21 -0500

    hatcheries per 1/4th min

--- JS/main.js
+++ JS/main.js
@@ -8,7 +8,7 @@ function mainLoop() {
     updateLayRate()
     updatePrestige()
     currentEggValue = eggValue[data.currentEgg].times(eggValueBonus)
-    data.chickens = data.chickens.plus(chickenGain.times(diff/60))
+    data.chickens = data.chickens.plus(chickenGain.times(diff/15))
     data.money = data.money.add(((currentEggValue.times(soulEggBoost)).mul(diff)).times(data.chickens.times(layRate)))
     updateHTML()
 }
--- JS/research.js
+++ JS/research.js
@@ -5,10 +5,10 @@ const commonResearchNames = ['Comfortable Nests','Nutritional Supplements','Inte
     'Genetic Purification','Machine Learning Incubators','Time Compression','Graviton Coating','Crystalline Shelling',
     'Neural Linking','Telepathic Will','Atomic Purification','Multiversal Layering','Timeline Diversion','Eggsistor Miniturization',
     'Matter Reconfiguration','Timeline Splicing','Relativity Optimization']
-const commonResearchDescs = ['Egg Laying Rate +10%','Egg Value +25%','+2 Chickens/min','Earnings Per Egg +25%',
-    'Doubles Egg Value','+5 Chickens/min','Triples Egg Value','Egg Laying Rate +5%','Egg Value +25%','+10 Chickens/min',
-    'Egg Laying Rate & Egg Value + 15%','Egg Value +15%','Doubles Egg Value','+25 Chickens/min','Egg Value +10%',
-    '+5 Chickens/min','Egg Laying Rate +10%','Double Egg Density (Value)','Egg Value +25%','+50 Chickens/min','Egg Quality (Value) +25%',
+const commonResearchDescs = ['Egg Laying Rate +10%','Egg Value +25%','+8 Chickens/min','Earnings Per Egg +25%',
+    'Doubles Egg Value','+20 Chickens/min','Triples Egg Value','Egg Laying Rate +5%','Egg Value +25%','+40 Chickens/min',
+    'Egg Laying Rate & Egg Value + 15%','Egg Value +15%','Doubles Egg Value','+100 Chickens/min','Egg Value +10%',
+    '+20 Chickens/min','Egg Laying Rate +10%','Double Egg Density (Value)','Egg Value +25%','+200 Chickens/min','Egg Quality (Value) +25%',
     'Egg Value +10%','10x Egg Value','Egg Laying Rate +2%','Egg Value +5%','Increases Egg Value +1%','10x Egg Value','Egg Laying Rate +10%']
 const commonResearchMaxLevel = [D(50),D(40),D(10),D(30),D(1),D(10),D(1),D(50),D(35),D(15),D(30),D(60),D(5),D(30),D(100),D(250),D(20),
     D(7),D(100),D(30),D(50),D(3),D(50),D(25),D(100),D(500),D(1),D(10)]
