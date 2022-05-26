d543ec4889db7bd936b03e00946207600ae73a4f
Dimava <dimava2@ya.ru>
2022-05-26 19:11:13 +0300

    fix /0 errors

--- JS/update.js
+++ JS/update.js
@@ -6,8 +6,8 @@ function updateHTML() {
         DOMCacheGetOrSet('currentEggImgHeader').setAttribute('src', `Imgs/${eggData[data.currentEgg].id}.png`)
     DOMCacheGetOrSet('eggPromoteButton').classList = data.money.gte(eggData[data.currentEgg].unlockReq) ? 'unlocked' : 'locked'
     DOMCacheGetOrSet('eggPromoteButton').style.display = data.currentEgg >= eggData.length-1 || contractActive() ? 'none' : 'inline-block'
-    const previousEggUnlockReq = data.currentEgg !== 0 ? eggData[data.currentEgg - 1].unlockReq : D(1)
-    const currentEggUnlockReq = eggData[data.currentEgg].unlockReq
+    const previousEggUnlockReq = data.currentEgg !== 0 ? eggData[data.currentEgg - 1].unlockReq.max(1.01) : D(1.01)
+    const currentEggUnlockReq = eggData[data.currentEgg].unlockReq.max(1.02)
     const lastEggUnlockProgress = data.money.max(1).log10().div(previousEggUnlockReq.log10())
     const nextEggUnlockProgress = data.money.div(previousEggUnlockReq).max(1).log10().div(currentEggUnlockReq.div(previousEggUnlockReq).log10())
     DOMCacheGetOrSet('eggPromoteButton').style.setProperty("--y", lastEggUnlockProgress.mul(100).max(0).min(100).toString() + '%')
