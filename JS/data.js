afd357c9c486dea2b164baa67733282800b6ee6b
MrBacon470 <Mrbacon470@outlook.com>
2022-06-18 11:08:47 -0500

    offline time notification

--- JS/data.js
+++ JS/data.js
@@ -140,6 +140,8 @@ window.setInterval(function(){
 }, 30000);
 window.onload = function (){
     load()
+    diff = diff = (Date.now()-data.time)*data.devSpeed/1000
+    $.notify('Welcome Back!\nYou were gone for ' + formatTime(diff), 'info')
     changeTab(data.currentTab)
     for(let i = 0; i < data.currentSubTab.length; i++) {
         changeSubTab(i,data.currentSubTab[i])
