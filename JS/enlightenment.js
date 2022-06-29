6ad380d6fe4d11b2e9c32c6a5ab935d122787353
MrBacon470 <Mrbacon470@outlook.com>
2022-06-29 13:22:10 -0500

    v1.1.4 finished up

--- JS/enlightenment.js
+++ JS/enlightenment.js
@@ -38,7 +38,11 @@ function enterPath() {
 }
 
 function purchaseEnlightenment(i) {
-    if(data.knowledge.lt(enlightenmentCosts[i])) return
-    data.knowledge = data.knowledge.minus(enlightenmentCosts[i])
-    data.enlightenments[i] = data.enlightenments[i].plus(1)
+    const buyAmountNums = [1,5,10,20]
+    for(let j = 0; j < buyAmountNums[data.buyAmounts[2]]; j++) {
+        if(data.knowledge.lt(enlightenmentCosts[i])) return
+        data.knowledge = data.knowledge.minus(enlightenmentCosts[i])
+        data.enlightenments[i] = data.enlightenments[i].plus(1)
+        updateEnlightenment()
+    }
 }
