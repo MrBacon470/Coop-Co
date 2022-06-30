88821b8610dbf6020238c3571703e863837bea41
MrBacon470 <Mrbacon470@outlook.com>
2022-06-30 09:38:30 -0500

    two more tickers

--- JS/newsticker.js
+++ JS/newsticker.js
@@ -30,7 +30,8 @@ newsArray = [//always true
     ['which came first, the egg or the egg?','true'],['"Genetic Purification"...? Isn\'t that just eugenics?','true'],['Florida man has broken several walls, the costs are too expensive to fix! (idk)','true'],['This game takes only a few moments to beat if you know how to use the console.','true'],
     ['“We quit, why should we be here making actual news when our neighboring news provider managed to have 200x more subscribers just by constantly churning out useless egg puns instead?” -The New Yolk Times ','true'],['Have you noticed that the browser icon of the game changes depending on what kind of egg you\'re currently producing?','true'],
     ['You might have noticed that the news ticker reports some of the news earlier than you\'d expect, but since our journalists have really limited programming skills, consider us time travelers. ','true'],['BREAKING NEWS: scientists believe we are living in a giant egg!','true'],
-    ['It\'s eggcellent how much progress you\'re making!'],['The next statement is true. The previous statement is false.','true'],
+    ['It\'s eggcellent how much progress you\'re making!'],['The next statement is true. The previous statement is false.','true'],['All of the king\'s horses and all of the king\'s men could have put Humpty Dumpty together again if he was made out of Supermaterial','true'],['The chance of seeing this news message is the chance of getting two egg yolks in one egg! (1/1000 chance)','getRandom(1,1000) === 1000'],
+    
 ];}
 
 let s = DOMCacheGetOrSet('news');
