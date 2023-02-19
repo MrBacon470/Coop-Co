57d557d8e5463b38b4ee16e6f65c319523928eaf
MrBacon470 <Mrbacon470@outlook.com>
2023-02-19 14:12:25 -0600

    convert achievement data to all objs

--- JS/achievements.js
+++ JS/achievements.js
@@ -1,42 +1,222 @@
-const achievementNames = [
-//Egg Discovery Section
-'Eggcellent','Superegg','Meggdical','Rockegg Fuel','Uber Material','Fused','Quantum Flux','Fountain of Youth','Time and Space',
-'Gravity Well','Diliuted','Prodigy Child','Terraforming Mars','Anti Anti Matter','Moar Matter','AEgg','Nebulous','Universal Problem','Egglightenment',
-//Contracts Section
-'Contracted','Contractual','Contractful','Contractor',
-//Soul and Prophecy Section
-'Soulful I','Soulful II','Soulful III','Soulful IV','Prophet I','Prophet II','Prophet III','Prophet IV',
-//Planets and Knowledge
-'Planetary I','Planetary II','Planetary III','Planetary IV','Planetary V','Planetary VI',
-'Intelligent','Genius','Einstein','True Egglightenment'
-]
-
-const achievementDescriptions = [
-//Egg Discovery Section
-'Start Playing!','Unlock the 2nd Egg','Unlock the 3rd Egg','Unlock the 4th Egg','Unlock the 5th Egg','Unlock the 6th Egg','Unlock the 7th Egg',
-'Unlock the 8th Egg','Unlock the 9th Egg','Unlock the 10th Egg','Unlock the 11th Egg','Unlock the 12th Egg','Unlock the 13th Egg','Unlock the 14th Egg','Unlock the 15th Egg',
-'Unlock the 16th Egg','Unlock the 17th Egg','Unlock the 18th Egg','Unlock the 19th Egg',
-//Contracts Section
-'Completed a Contract','Complete 10 Contracts','Complete 100 Contracts','Complete a Thousand Contracts',
-//Soul and Prophecy Section
-'Get 1 Thousand Soul Eggs','Get 1 Million Soul Eggs','Get 1 Billion Soul Eggs','Get 1 Trillion Soul Eggs',
-'Get 10 Prophecy Eggs','Get 1 Thousand Prophecy Eggs','Get 1 Million Prophecy Eggs','Get 10 Million Prophecy Eggs',
-//Planets and Knowledge
-'Unlock the 1st Planet','Unlock the 2nd Planet','Unlock the 3rd Planet','Unlock the 4th Planet','Unlock the 5th Planet','Unlock the 6th Planet',
-'Acquire 100 Knowledge','Acquire 1 Thousand Knowledge','Acquire 10 Thousand Knowledge','Acquire 100 Thousand Knowledge'
+const achievementObjs = [
+    {
+        name: 'Eggcellent',
+        description: 'Start Playing!',
+        img: `${eggImgPath}${eggData[0].id}.png`
+    },
+    {
+        name: 'Superegg',
+        description: 'Unlock the 2nd Egg',
+        img: `${eggImgPath}${eggData[1].id}.png`
+    },
+    {
+        name: 'Meggdical',
+        description: 'Unlock the 3rd Egg',
+        img: `${eggImgPath}${eggData[2].id}.png`
+    },
+    {
+        name: 'Rockegg Fuel',
+        description: 'Unlock the 4th Egg',
+        img: `${eggImgPath}${eggData[3].id}.png`
+    },
+    {
+        name: 'Uber Material',
+        description: 'Unlock the 5th Egg',
+        img: `${eggImgPath}${eggData[4].id}.png`
+    },
+    {
+        name: 'Fused',
+        description: 'Unlock the 6th Egg',
+        img: `${eggImgPath}${eggData[5].id}.png`
+    },
+    {
+        name: 'Quantum Flux',
+        description: 'Unlock the 7th Egg',
+        img: `${eggImgPath}${eggData[6].id}.png`
+    },
+    {
+        name: 'Fountain of Youth',
+        description: 'Unlock the 8th Egg',
+        img: `${eggImgPath}${eggData[7].id}.png`
+    },
+    {
+        name: 'Time and Space',
+        description: 'Unlock the 9th Egg',
+        img: `${eggImgPath}${eggData[8].id}.png`
+    },
+    {
+        name: 'Gravity Well',
+        description: 'Unlock the 10th Egg',
+        img: `${eggImgPath}${eggData[9].id}.png`
+    },
+    {
+        name: 'Diliuted',
+        description: 'Unlock the 11th Egg',
+        img: `${eggImgPath}${eggData[10].id}.png`
+    },
+    {
+        name: 'Prodigy Child',
+        description: 'Unlock the 12th Egg',
+        img: `${eggImgPath}${eggData[11].id}.png`
+    },
+    {
+        name: 'Terraforming Mars',
+        description: 'Unlock the 13th Egg',
+        img: `${eggImgPath}${eggData[12].id}.png`
+    },
+    {
+        name: 'Anti Anti Matter',
+        description: 'Unlock the 14th Egg',
+        img: `${eggImgPath}${eggData[13].id}.png`
+    },
+    {   
+        name: 'Moar Matter',
+        description: 'Unlock the 15th Egg',
+        img: `${eggImgPath}${eggData[14].id}.png`
+    },
+    {
+        name: 'AEgg',
+        description: 'Unlock the 16th Egg',
+        img: `${eggImgPath}${eggData[15].id}.png`
+    },
+    {
+        name: 'Nebulous',
+        description: 'Unlock the 17th Egg',
+        img: `${eggImgPath}${eggData[16].id}.png`
+    },
+    {
+        name: 'Universal Problem',
+        description: 'Unlock the 18th Egg',
+        img: `${eggImgPath}${eggData[17].id}.png`
+    },
+    {
+        name: 'Egglightenment',
+        description: 'Unlock the 19th Egg',
+        img: `${eggImgPath}${eggData[18].id}.png`
+    },
+    {
+        name: 'Contracted',
+        description: 'Completed a Contract',
+        img: 'Imgs/greencheck.png'
+    },
+    {
+        name: 'Contractual',
+        description: 'Complete 10 Contracts',
+        img: 'Imgs/greencheck.png'
+    },
+    {
+        name: 'Contractful',
+        description: 'Complete 100 Contracts',
+        img: 'Imgs/greencheck.png'
+    },
+    {
+        name: 'Contractor',
+        description: 'Complete a Thousand Contracts',
+        img: 'Imgs/greencheck.png'
+    },
+    {
+        name: 'Soulful I',
+        description: 'Get 1 Thousand Soul Eggs',
+        img: `${eggImgPath}soul.png`
+    },
+    {
+        name: 'Soulful II',
+        description: 'Get 1 Million Soul Eggs',
+        img: `${eggImgPath}soul.png`
+    },
+    {
+        name: 'Soulful III',
+        description: 'Get 1 Billion Soul Eggs',
+        img: `${eggImgPath}soul.png`
+    },
+    {
+        name: 'Soulful IV',
+        description: 'Get 1 Trillion Soul Eggs',
+        img: `${eggImgPath}soul.png`
+    },
+    {
+        name: 'Prophet I',
+        description: 'Get 10 Prophecy Eggs',
+        img: `${eggImgPath}prophecy.png`
+    },
+    {
+        name: 'Prophet II',
+        description: 'Get 100 Prophecy Eggs',
+        img: `${eggImgPath}prophecy.png`
+    },
+    {
+        name: 'Prophet III',
+        description: 'Get 1 Thousand Prophecy Eggs',
+        img: `${eggImgPath}prophecy.png`
+    },
+    {
+        name: 'Prophet IV',
+        description: 'Get 10 Thousand Prophecy Eggs',
+        img: `${eggImgPath}prophecy.png`
+    },
+    {
+        name: 'Planetary I',
+        description: 'Unlock the 1st Planet',
+        img: `Imgs/planet1.png`
+    },
+    {
+        name: 'Planetary II',
+        description: 'Unlock the 2nd Planet',
+        img: `Imgs/planet2.png`
+    },
+    {
+        name: 'Planetary III',
+        description: 'Unlock the 3rd Planet',
+        img: `Imgs/planet3.png`
+    },
+    {
+        name: 'Planetary IV',
+        description: 'Unlock the 4th Planet',
+        img: `Imgs/planet4.png`
+    },
+    {
+        name: 'Planetary V',
+        description: 'Unlock the 5th Planet',
+        img: `Imgs/planet5.png`
+    },
+    {
+        name: 'Planetary VI',
+        description: 'Unlock the 6th Planet',
+        img: `Imgs/planet6.png`
+    },
+    {
+        name: 'Intelligent',
+        description: 'Acquire 100 Knowledge',
+        img: `Imgs/lightbulb.png`
+    },
+    {
+        name: 'Genius',
+        description: 'Acquire 1 Thousand Knowledge',
+        img: `Imgs/lightbulb.png`
+    },
+    {
+        name: 'Einstein',
+        description: 'Acquire 10 Thousand Knowledge',
+        img: `Imgs/lightbulb.png`
+    },
+    {
+        name: 'True Egglightenment',
+        description: 'Acquire 100 Thousand Knowledge',
+        img: `Imgs/lightbulb.png`
+    },
 ]
 
 function updateAchText(i) {
-    DOMCacheGetOrSet('achHoverText').innerText = `[${i+1}] - ${achievementNames[i]}\n${achievementDescriptions[i]}`
+    DOMCacheGetOrSet('achHoverText').innerText = `[${i+1}] - ${achievementObjs[i].name}\n${achievementObjs[i].desc}`
 }
 
-for(let i = 0; i < achievementNames.length; i++) 
+for(let i = 0; i < achievementObjs.length; i++) 
     DOMCacheGetOrSet('ach' + i).addEventListener('mouseover', () => updateAchText(i))
 
 function getAchievement(i) {
     if(data.achievements[i] === true) return
     data.achievements[i] = true
-    $.notify(`${achievementNames[i]} Unlocked!`,'success')
+    $.notify(`${achievementObjs[i].name} Unlocked!`,'success')
     updateAchClass()
 }
 
@@ -66,7 +246,7 @@ function checkAchievements() {
 }
 
 function updateAchClass() {
-    for(let i = 0; i < achievementNames.length; i++) {
+    for(let i = 0; i < achievementObjs.length; i++) {
             DOMCacheGetOrSet('ach'+i).classList = data.achievements[i] ? 'achUnlock' : 'achLock'
     }
 
