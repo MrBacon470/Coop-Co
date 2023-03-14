cb390931760825cfa754fe3dd9891c01a2f40d75
MrBacon470 <Mrbacon470@outlook.com>
2023-03-13 19:33:01 -0500

    Das Gems

---
+++ JS/ascension.js
@@ -0,0 +1,67 @@
+const gemObjs = [
+    {
+        name: 'Normal Gem',
+        src: 'Imgs/Gems/gem.png',
+    },
+    {
+        name: 'Prestige Gem',
+        src: 'Imgs/Gems/prestigeGem.png',
+    },
+    {
+        name: 'Ascension Gem',
+        src: 'Imgs/Gems/ascensionGem.png',
+    },
+    {
+        name: 'Tachyon Gem',
+        src: 'Imgs/Gems/tachyonGem.png',
+    },
+    {
+        name: 'Dark Matter Gem',
+        src: 'Imgs/Gems/darkmatterGem.png',
+    },
+    {
+        name: 'Antimatter Gem',
+        src: 'Imgs/Gems/antimatterGem.png',
+    },
+    {
+        name: 'Nebula Gem',
+        src: 'Imgs/Gems/nebulaGem.png',
+    },
+    {
+        name: 'Soul Gem',
+        src: 'Imgs/Gems/soulGem.png',
+    },
+    {
+        name: 'Void Gem',
+        src: 'Imgs/Gems/voidGem.png',
+    },
+    {
+        name: 'Quantum Gem',
+        src: 'Imgs/Gems/quantumGem.png',
+    },
+    {
+        name: 'Universe Gem',
+        src: 'Imgs/Gems/universeGem.png',
+    },
+    {
+        name: 'Knowledge Gem',
+        src: 'Imgs/Gems/knowledgeGem.png',
+    },
+    {
+        name: 'Χ Gem',
+        src: 'Imgs/Gems/chiGem.png',
+    },
+    {
+        name: 'Ψ Gem',
+        src: 'Imgs/Gems/psiGem.png',
+    },
+    {
+        name: 'Ω Gem',
+        src: 'Imgs/Gems/omegaGem.png',
+    },
+    {
+        name: 'Singularity Gem',
+        src: 'Imgs/Gems/singularityGem.png',
+    }
+]
+
