5ca2e617704352c9555ac75ce295e9dd0ae5ffea
VoidCloud <72811949+MrBacon470@users.noreply.github.com>
2023-11-09 22:44:40 -0600

    Hiding and Showing new Help Menus

--- public/Scripts/update.js
+++ public/Scripts/update.js
@@ -66,9 +66,9 @@ function updateHTML() {
             DOMCacheGetOrSet('help1').style.display = data.hasPrestiged ? 'block' : 'none'
             DOMCacheGetOrSet('help2').style.display = data.unlockedEgg[3] ? 'block' : 'none'
             DOMCacheGetOrSet('help3').style.display = data.unlockedContracts ? 'block' : 'none'
-            // DOMCacheGetOrSet('help4').style.display = data.hasAscended ? 'block' : 'none'
-            // DOMCacheGetOrSet('help5').style.display = data.hasAscended ? 'block' : 'none'
-            // DOMCacheGetOrSet('help6').style.display = data.hasAscended ? 'block' : 'none'
+            DOMCacheGetOrSet('help4').style.display = data.hasAscended ? 'block' : 'none'
+            DOMCacheGetOrSet('help5').style.display = data.hasAscended ? 'block' : 'none'
+            DOMCacheGetOrSet('help6').style.display = data.legendaryResearch[3].gte(legendaryResearches[3].max) ? 'block' : 'none'
         }
     }
     else if(data.currentTab === 4) {
