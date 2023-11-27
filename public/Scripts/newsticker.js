cb3bce2c13ac4ad5952b72c6c491feb79794f196
VoidCloud <72811949+MrBacon470@users.noreply.github.com>
2023-11-27 16:20:57 -0600

    Revert Major Newsticker changes

--- public/Scripts/newsticker.js
+++ public/Scripts/newsticker.js
@@ -268,69 +268,54 @@ const newsArray = [
 
 ];
 
-const s = DOMCacheGetOrSet("news");
-document.addEventListener(
-    "visibilitychange",
-    () => {
-        if (!document.hidden) scrollNextMessage();
-    },
-    false
-);
-const scrollTimeouts = [];
-let nextMsgIndex = 0;
+let s = DOMCacheGetOrSet('news');
+document.addEventListener("visibilitychange", function() {if (!document.hidden) {scrollNextMessage();}}, false);
+let scrollTimeouts = [];
+let nextMsgIndex;
 function scrollNextMessage() {
-    //don't run if hidden to save performance
-    if (DOMCacheGetOrSet("newsHolder").style.display === "none") return;
-    let includeThisTicker = true;
-    //select a message at random
+  //don't run if hidden to save performance
+  if(DOMCacheGetOrSet('newsHolder').style.display === 'none') return
+  //select a message at random
 
-    do {
-        nextMsgIndex = Math.floor(Math.random() * newsArray.length);
-        includeThisTicker = newsArray[nextMsgIndex][1];
-    } while (
-        typeof includeThisTicker === "function"
-            ? includeThisTicker()
-            : includeThisTicker
-    );
-
-    scrollTimeouts.forEach(clearTimeout);
-    scrollTimeouts.length = 0;
-
-    const text = newsArray[nextMsgIndex][0];
-    //set the text
-    s.innerHTML = typeof text === "function" ? text() : text;
-
-    //get the parent width so we can start the message beyond it
-    const parentWidth = s.parentElement.clientWidth;
-
-    //set the transition to blank so the move happens immediately
-    s.style.transition = "";
-    //move div_text to the right, beyond the edge of the div_container
-    s.style.transform = `translateX(${parentWidth}px)`;
-
-    //we need to use a setTimeout here to allow the browser time to move the div_text before we start the scrolling
-    scrollTimeouts.push(
-        setTimeout(() => {
-            //distance to travel is s.parentElement.clientWidth + s.clientWidth + parent padding
-            //we want to travel at rate pixels per second so we need to travel for (distance / rate) seconds
-            const dist = s.parentElement.clientWidth + s.clientWidth + 20; //20 is div_container padding
-            const rate = 100; //change this value to change the scroll speed
-            const transformDuration = dist / rate;
-
-            //set the transition duration
-            s.style.transition = `transform ${transformDuration}s linear`;
-            const textWidth = s.clientWidth;
-            //we need to move it to -(width+parent padding) before it won't be visible
-            s.style.transform = `translateX(-${textWidth + 5}px)`;
-            //automatically start the next message scrolling after this one finishes
-            //you could add more time to this timeout if you wanted to have some time between messages
-            scrollTimeouts.push(
-                setTimeout(
-                    scrollNextMessage,
-                    Math.ceil(transformDuration * 1000)
-                )
-            );
-            newsArray[nextMsgIndex][2]?.();
-        }, 100)
-    );
+  try {
+    do {nextMsgIndex = Math.floor(Math.random() * newsArray.length)} while (!eval(newsArray[nextMsgIndex][1]))
+  } catch(e) {
+      console.log("Newsarray doesn't work at idx " + nextMsgIndex)
+  }
+  scrollTimeouts.forEach(function(v) {clearTimeout(v);});
+  scrollTimeouts = [];
+  
+  //set the text
+  s.innerHTML = newsArray[nextMsgIndex][0];
+  //get the parent width so we can start the message beyond it
+  let parentWidth = s.parentElement.clientWidth;
+  //set the transition to blank so the move happens immediately
+  s.style.transition = '';
+  //move div_text to the right, beyond the edge of the div_container
+  s.style.transform = 'translateX('+parentWidth+'px)';
+  //we need to use a setTimeout here to allow the browser time to move the div_text before we start the scrolling
+  scrollTimeouts.push(setTimeout( function() {
+    //distance to travel is s.parentElement.clientWidth + s.clientWidth + parent padding
+    //we want to travel at rate pixels per second so we need to travel for (distance / rate) seconds
+    let dist = s.parentElement.clientWidth + s.clientWidth + 20; //20 is div_container padding
+    let rate = 100; //change this value to change the scroll speed
+    let transformDuration = dist / rate;
+    /*
+    if (!player.options.newsHidden && !player.newsArray.includes(newsArray[nextMsgIndex][2])) {
+        player.newsArray.push(newsArray[nextMsgIndex][2]);
+        if (player.newsArray.length>=50) giveAchievement("Fake News")
+    }
+    */
+    //set the transition duration
+    s.style.transition = 'transform '+transformDuration+'s linear';
+    let textWidth = s.clientWidth;
+    //we need to move it to -(width+parent padding) before it won't be visible
+    s.style.transform = 'translateX(-'+(textWidth+5)+'px)';
+    //automatically start the next message scrolling after this one finishes
+    //you could add more time to this timeout if you wanted to have some time between messages
+    scrollTimeouts.push(setTimeout(scrollNextMessage, Math.ceil(transformDuration * 1000)));
+    if(newsArray[nextMsgIndex].length === 3) {
+      eval(newsArray[nextMsgIndex][2])
+    }
+  }, 100));
 }
