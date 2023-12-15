13f3a40c90fba3034db5ee08ab5aa224cdefe772
VoidCloud <72811949+MrBacon470@users.noreply.github.com>
2023-12-15 11:50:58 -0600

    trying stuff again :/

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://mrbacon470.github.io/Coop-Co",
-    base: "/Coop-Co",
-    //output: "static",
+    //base: "/Coop-Co",
+    output: "static",
 });
