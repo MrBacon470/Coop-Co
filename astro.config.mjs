cde55b5a990130dd692279428bc190d369752753
VoidCloud <72811949+MrBacon470@users.noreply.github.com>
2023-12-15 13:49:16 -0600

    config change fix again?

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://mrbacon470.github.io/Coop-Co",
-    base: "/coop-co",
+    site: "https://mrbacon470.github.io",
+    base: "/Coop-Co",
     output: "static"
 });
